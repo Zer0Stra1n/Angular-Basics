3176a7b9246201b1d46882cb48e799c135614fb8
Zer0Stra1n <zero@DESKTOP-QPEQQVR.localdomain>
2020-11-23 11:55:55 -0500

    added bootstrap and got it running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-basics';
+  scrollTop(){
+    window.scrollTo(0, 0);
+  }
 }
